b5413927fb36412c5e262e6d11ff657fdc063f9a
laura <lnelson100@comcast.net>
2025-02-07 15:57:21 -0600

    database progress

--- backend/root/user_management/admin.py
+++ backend/root/user_management/admin.py
@@ -5,63 +5,11 @@ from user_management.models import User
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
+from .models import User, Plants, Gardens, Garden_log
+
+admin.site.register(User)
 admin.site.unregister(Group)
+admin.site.register(Plants)
+admin.site.register(Gardens)
+admin.site.register(Garden_log)
 
-
-class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ('email', 'first_name', 'last_name', 'role', 'address')
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
-class UserChangeForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('email', 'first_name', 'last_name', 'is_active', 'is_superuser', 'role', 'address')
-
-
-class UserAdmin(BaseUserAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
-    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active')
-    list_filter = ('is_active', 'role')
-    fieldsets = (
-        (None, {'fields': ('email',)}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'role', 'address', 'is_active')}),
-        ('Permissions', {'fields': ('is_superuser',)}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'role', 'address', 'password1', 'password2')}
-        ),
-    )
-    search_fields = ('email', 'epantherid', 'first_name', 'last_name')
-    ordering = ('email',)
-    filter_horizontal = ()
-
-    def get_form(self, request, obj=None, **kwargs):
-        if obj is None:
-            return UserCreationForm
-        else:
-            return UserChangeForm
-
-
-admin.site.register(User, UserAdmin)
--- backend/root/user_management/models.py
+++ backend/root/user_management/models.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from django.core.validators import MinLengthValidator
+from django.db.models import UniqueConstraint, CheckConstraint, Q
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -22,27 +25,27 @@ class UserManager(BaseUserManager):
 
         return self.create_user(email, password, **extra_fields)
 
+class Roles(models.TextChoices):
+    AD = "admin"
+    US = "user"
+    MO = "moderator"
+
+
+class Sun_levels(models.TextChoices):
+    FULLSUN = "full sun"
+    PARTSUN = "partial sun"
+    PARTSHADE = "partial shade"
+    FULLSHADE = "full shade"
+
 
 class User(AbstractBaseUser, PermissionsMixin):
-    ROLE_CHOICES = (
-        ('Supervisor', 'Supervisor/Administrator'),
-        ('User', 'User'),
-    )
-
     email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first name'), max_length=150, blank=True)
-    last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    role = models.CharField(_('role'), max_length=50, choices=ROLE_CHOICES, default='User', help_text=_('User role in the system'))
-    address = models.CharField(_('address'), max_length=255, blank=True, null=True)
+    user_password = models.CharField(_('password'), max_length=100, blank=True, null=True)
+    first_name = models.CharField(_('first name'), max_length=50)
+    last_name = models.CharField(_('last name'), blank=True, null=True, max_length=50)
+    role = models.CharField(_('role'), max_length=9, choices=Roles.choices, default=Roles.US)
     is_active = models.BooleanField(_('active'), default=True)
-    epantherid = models.CharField(_('ePanther ID'), max_length=20, default='jbartosz')
-
-    # email = models.EmailField(_('email address'), unique=True)
-    # first_name = models.CharField(_('first name'), max_length=150, blank=True)
-    # last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    # user_id = models.CharField(_('user id'), max_length=20, blank=True, null=True)
-    # user_password = models.CharField(_('user password'), max_length=20, blank=True, null=True)
-    # zip_code = models.CharField(_('zip code'), max_length=5, blank=True, null=True)
+    zip_code = models.CharField(_('zip code'), blank=True, null=True, max_length=5, validators=[MinLengthValidator(5)])
 
     objects = UserManager()
 
@@ -55,7 +58,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = _('users')
 
     def __str__(self):
-        return self.email
+        return f"{self.email} id:{self.id}"
 
     def get_full_name(self):
         full_name = f'{self.first_name} {self.last_name}'.strip()
@@ -73,3 +76,106 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def has_module_perms(self, app_label):
         return self.is_superuser
+
+
+class Plants(models.Model):
+    species = models.CharField(max_length=50)
+    variety = models.CharField(blank=True, null=True, max_length=50)
+    maturity_time = models.IntegerField() # in number of days
+    # possible other attributes
+    germination_time = models.IntegerField(blank=True, null=True) # in number of days
+    spacing_x = models.IntegerField(blank=True, null=True) # in inches
+    spacing_y = models.IntegerField(blank=True, null=True) # in inches
+    sun_level = models.CharField(blank=True, null=True, max_length=13, choices=Sun_levels.choices)
+    planting_depth = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2) # in inches
+    water_req = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2) # in inches
+    plant_description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                check = Q(maturity_time__gte=0), 
+                name = 'check_maturity_pos',
+            ),
+
+            CheckConstraint(
+                check = Q(germination_time__gte=0 or Q(germination_time__isnull=True)), 
+                name = 'check_germ_pos',
+            ),
+
+            CheckConstraint(
+                check = Q(spacing_x__gte=0 or Q(spacing_x__isnull=True)), 
+                name = 'check_spacing_x_pos',
+            ),
+
+            CheckConstraint(
+                check = Q(spacing_y__gte=0 or Q(spacing_y__isnull=True)), 
+                name = 'check_spacing_y_pos',
+            ),
+
+            CheckConstraint(
+                check = Q(planting_depth__gte=0 or Q(planting_depth__isnull=True)), 
+                name = 'check_depth_pos',
+            ),
+
+            CheckConstraint(
+                check = Q(water_req__gte=0 or Q(water_req__isnull=True)), 
+                name = 'check_water_pos',
+            ),
+        ]
+    
+    def __str__(self):
+        return f"{self.species} - {self.variety}"
+
+
+class Gardens(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    size_x = models.IntegerField() # in inches
+    size_y = models.IntegerField() # in inches
+
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                check = Q(size_x__gt=0), 
+                name = 'check_size_x_pos',
+            ),
+
+            CheckConstraint(
+                check = Q(size_y__gt=0), 
+                name = 'check_size_y_pos',
+            ),
+        ]
+    
+    def __str__(self):
+        return f"user id:{self.user_id} - size:{self.size_x}x{self.size_y}"
+
+
+class Garden_log(models.Model):
+    garden_id = models.ForeignKey(Gardens, on_delete=models.CASCADE)
+    plant_id = models.ForeignKey(Plants, on_delete=models.DO_NOTHING)
+    # Note: planted_date should be user entered rather than auto-generated/timestamp.
+    planted_date = models.DateField()
+    x_coordinate = models.IntegerField() # in inches
+    y_coordinate = models.IntegerField() # in inches
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['id', 'x_coordinate', 'y_coordinate'],
+                name='unq_plot_space'
+            ),
+
+            CheckConstraint(
+                check = Q(x_coordinate__gte=0), 
+                name = 'check_x_coor_pos',
+            ),
+
+            CheckConstraint(
+                check = Q(y_coordinate__gte=0), 
+                name = 'check_y_coor_pos',
+            ),
+        ]
+    
+    def __str__(self):
+        return f"garden:{self.garden_id} - plant:{self.plant_id} @ [{self.x_coordinate},{self.y_coordinate}]"
+    
