d79bc5b5454cb3a7142c53e1f7940159bdacc1bb
Julian Bartosz <thejulianbartosz@gmail.com>
2025-02-17 17:33:06 -0600

    Update admin.py with Laura's changes

--- backend/root/user_management/admin.py
+++ backend/root/user_management/admin.py
@@ -5,11 +5,13 @@ from user_management.models import User
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from .models import User, Plants, Gardens, Garden_log
+from .models import User, Plants, Gardens, Garden_log, Forums, Replies, Likes
 
 admin.site.register(User)
 admin.site.unregister(Group)
 admin.site.register(Plants)
 admin.site.register(Gardens)
 admin.site.register(Garden_log)
-
+admin.site.register(Forums)
+admin.site.register(Replies)
+admin.site.register(Likes)
