72ab21efa5f986b6c07e0d7a3d4641623a1c3e32
Beckett Frey <beckett.frey@gmail.com>
2025-02-18 17:54:36 -0600

    Ready for presentation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import './App.css'
 import Garden from './components/GardenSection/Garden'
 import NavBar from './components/NavBarSection/NavBar';
@@ -6,48 +8,30 @@ import SearchPlants from './components/SearchSection/SearchPlants'
 
 function App() {
 
-  // Placeholder
-  const cells = [
-    ["🍅", "🥕", "🌽", "🍆", "🥦"],
-    ["🌳", "🌲", null, null, null],
-    Array(5).fill(null),
-    Array(5).fill(null),
-    Array(5).fill(null),
-  ];
-
-  // TODO: Retrieve username
+  // TODO: Retrieve user info and authentication token from login redirect
   const username = "Johnny Appleseed";
 
-  // TODO: Retrieve Gardens
-  const gardens = [{name: 'Garden 1', x: 5, y: 5, cells: cells}, {name: 'Garden 2', x: 5, y: 5, cells: cells}, {name: 'Garden 3', x:5, y: 5, cells: cells}]; 
-
-  // TODO: Retrieve Plants matching search
-  const plantslist = [
-    { name: "🌽", description: "Description 1" },
-    { name: "🥦", description: "Description 2" },
-    { name: "🥕", description: "Description 3" },
-    { name: "🌳", description: "Description 4" },
-  ];
-
   return (
-     <div className= 'app' style={{backgroundColor: 'white', width: '100vw', height: '100vh',position: 'relative' }}>
-     <NavBar username={username}/> 
-      <div className="sidebar" style={{
-        position: 'fixed', top: '60px', left: 0, width: '200px', height: 'calc(100vh - 60px)',
-        background: 'grey', padding: '10px', zIndex: 5
-      }}> 
-      <SearchPlants plants={plantslist} />
-      </div>
-      <div className="content" style={{
-        position: 'fixed', top: '60px', left: '240px', width: 'calc(100vw - 200px)',
-        height: 'calc(100vh - 60px)', background: '#eee',
-        display: 'flex', justifyContent: 'center', alignItems: 'center'
-      }}>
-        <Garden gardens={gardens} />
-      </div>
-    </div>  
-
-)
+    <div className='app' style={{ backgroundColor: 'white', width: '100vw', height: '100vh', position: 'relative' }}>
+      <NavBar username={username} />
+      <DndProvider backend={HTML5Backend}>
+        <div className="sidebar" style={{
+          position: 'fixed', top: '60px', left: 0, width: '200px', height: 'calc(100vh - 60px)',
+          background: 'linear-gradient(to right, rgb(152, 152, 152),rgb(65, 64, 64))', padding: '10px', zIndex: 5
+          , borderRadius: '0 10px 0 0'
+        }}>
+          <SearchPlants username={username} />
+        </div>
+        <div  style={{
+          position: 'fixed', top: '60px', left: '240px', width: 'calc(100vw - 200px)',
+          height: 'calc(100vh - 60px)', background: 'white',
+          display: 'flex', justifyContent: 'center', alignItems: 'center',
+        }}>
+          <Garden username={username} />
+        </div>
+      </DndProvider>
+    </div>
+  )
 }
 
 export default App;
--- frontend/src/components/SearchSection/SearchPlants.jsx
+++ frontend/src/components/SearchSection/SearchPlants.jsx
@@ -1,39 +1,71 @@
-import React, { useState } from 'react';
-import { DndProvider, useDrag, useDrop } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import React, { useState, useEffect } from 'react';
+import { useDrag } from 'react-dnd';
 import './SearchPlants.css';
 
-const SearchPlants = ({ plants }) => {
+const fetchPlants = (searchTerm) => {
+
+//     try {
+//         const response = await fetch('/api/plants/{searchTerm}');
+//         const data = await response.json();
+//         return data;
+//     } catch (error) {
+//         console.error('Error fetching plants:', error);
+//         return [];
+//     }
+// };
+
+// TODO: Retrieve Plants matching search
+    let plantslist = [
+        { id: 19, name: "🌽", description: "Corn" },
+        { id: 29, name: "🥦", description: "Broc" },
+        { id: 39, name: "🥕", description: "Carrot" },
+        { id: 49, name: "🌳", description: "Tree" },
+    ];
+
+    return plantslist;
+};
+
+
+const SearchPlants = ({ username }) => {
+
+
     const [searchTerm, setSearchTerm] = useState('');
-    const [filteredPlants, setFilteredPlants] = useState(plants);
+    const [filteredPlants, setFilteredPlants] = useState(fetchPlants(searchTerm));
+
+    useEffect(() => {
+        if (searchTerm) {
+            setFilteredPlants(fetchPlants(searchTerm))
+        }
+    }, [searchTerm]);
 
     const handleSearch = (event) => {
         const term = event.target.value.toLowerCase();
         setSearchTerm(term);
-        setFilteredPlants(plants.filter(plant => plant.name.toLowerCase().includes(term)));
     };
-
+    
     return (
-        <div className="search-plants-container">
+        <div>
             <input
                 type="text"
-                placeholder="Search plants..."
+                placeholder="🔍 Search plants"
                 value={searchTerm}
                 onChange={handleSearch}
                 className="search-input"
+                style={{ color: 'black', backgroundColor: 'white', marginBottom: '10px' }}  
             />
-            <DndProvider backend={HTML5Backend} >
-                <PlantList plants={filteredPlants} />
-            </DndProvider>
+
+            <PlantList plants={filteredPlants} />
         </div>
     );
 };
+
 
 const PlantList = ({ plants }) => {
     return (
         <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%', height: 'calc(100% - 40px)', overflowY: 'auto' }}>
             {plants.map((plant, index) => (
-                <div key={plant.id} style={{ flex: '1 0 50%', boxSizing: 'border-box', padding: '10px' }}>
+                
+                <div key={plant.id} style={{ fontSize: '21px', flex: '1 0 50%', boxSizing: 'border-box', padding: '10px' }}>
                     <PlantItem plant={plant} index={index} />
                 </div>
             ))}
@@ -41,10 +73,11 @@ const PlantList = ({ plants }) => {
     );
 };
 
+
 const PlantItem = ({ plant, index }) => {
     const [, ref] = useDrag({
         type: 'PLANT',
-        item: { id: plant.id, index }
+        item: plant
     });
 
     return (
