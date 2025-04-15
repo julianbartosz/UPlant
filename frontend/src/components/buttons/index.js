ed68713fd38740a68dc86cf6d57039529047d65c
Beckett Frey <beckett.frey@gmail.com>
2025-04-14 19:58:32 -0500

    Refactored function and updated styling of components broadly and documented dependency

--- frontend/src/components/buttons/index.js
+++ frontend/src/components/buttons/index.js
@@ -4,6 +4,7 @@ export { default as CircleButton } from './CircleButton';
 export { default as DeleteButton } from './DeleteButton';
 export { default as GardenButton } from './GardenButton';
 export { default as TextButton } from './TextButton';
+export { default as GenericButton } from './GenericButton';
 
 
 
