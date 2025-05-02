689aaa1c7bba567f77b7de321ac3ab2f674842a3
Beckett Frey <beckett.frey@gmail.com>
2025-05-02 18:58:34 -0500

    Cleaning file structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { DashboardPage, CatalogPage, SettingsPage, NotificationsPage } from './pages';
+import { DashboardPage, NotificationsPage, CatalogPage, SettingsPage } from './pages';
+import { Navigate } from 'react-router-dom';
 import UserProvider from './context/UserProvider';
-import { DashboardPage } from './pages';
 import './styles/app.css';
-import { useState, useEffect } from 'react';
-import { NotificationList } from './components/layout';
-import { UserContext } from './context/UserProvider';
-import NotificationsPage from './pages/NotificationsPage';
-
-import SettingsPage from './pages/SettingsPage';
 
 function App() {
-
-const fetchDetails = async () => {
-  const response = await fetch('http://localhost:8000/api/notifications/notifications/5688/', {
-    method: 'GET',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-  const data = await response.json();
-  console.log("DATA((((((((",data);
-};
-
-
 
   return (
     <UserProvider>
       <Router basename='/app'>
         <Routes>
-          <Route path="/fetch-details" element={
-            <div className="centered-message">
-              <button onClick={fetchDetails}>Fetch Details</button>
-              <NotificationList />
-            </div>
-          } />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<DashboardPage />} />
-          {/* <Route path="/catalog" element={<CatalogPage />} />  */}
+          <Route path="/catalog" element={<CatalogPage />} /> 
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="/notifications" element={<NotificationsPage />} />
           <Route path="*" element={
